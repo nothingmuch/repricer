dbdeeae282db908370cc74241c2ae0e4d5708634
Yuval Kogman <nothingmuch@woobling.org>
2020-01-16 19:32:34 +0000

    Stub healthcheck HTTP handler
    Currently meaningless, added to try k8s features, but will be
    semantically meaningful soon enough due to slow start of in memory last
    known prices index.

--- main.go
+++ main.go
@@ -7,7 +7,19 @@ import (
 )
 
 func main() {
-	http.Handle("/api/reprice", handlers.Reprice)
-	http.Handle("/api/product/", handlers.Product)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	apiMux := http.NewServeMux()
+	apiMux.Handle("/api/reprice", handlers.Reprice)
+	apiMux.Handle("/api/product/", handlers.Product)
+
+	go func() {
+		// just a fake set of healthchecks since the app currently entirely statless
+		backplaneMux := http.NewServeMux()
+		alwaysOK := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) { w.WriteHeader(http.StatusOK) })
+		backplaneMux.Handle("/healthz/alive", alwaysOK)
+		backplaneMux.Handle("/healthz/ready", alwaysOK)
+		_ = http.ListenAndServe(":9102", backplaneMux)
+	}()
+
+	log.Fatal(http.ListenAndServe(":8080", apiMux))
+
 }
