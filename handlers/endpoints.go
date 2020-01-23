679094e20cfb8d8b35ac242caeba3b13dda2b17f
Yuval Kogman <nothingmuch@woobling.org>
2020-01-23 18:31:42 +0000

    Add throttling to query and product endpoints
    Uses a very simple buffered chan struct{} as a semaphore.
    
    Note that I cannot get this to actually fail using apachebench without
    also adding significant (~ 10*time.Millisecond) delays into the
    ServeHTTP handler.

--- handlers/endpoints.go
+++ handlers/endpoints.go
@@ -24,8 +24,29 @@ func API(m Model) http.Handler {
 	apiMux := http.NewServeMux()
 
 	apiMux.Handle("/api/reprice", Reprice(m))
-	apiMux.Handle("/api/product/", Product(m))
-	apiMux.Handle("/api/query", Query(m))
+	apiMux.Handle("/api/product/", throttle(Product(m), 50))
+	apiMux.Handle("/api/query", throttle(Query(m), 50))
 
 	return apiMux
+}
+
+func throttle(h http.Handler, inFlight int) http.Handler {
+	return semaphoreHandler{h, make(chan struct{}, inFlight)}
+}
+
+type semaphoreHandler struct {
+	http.Handler
+	semaphore chan struct{}
+}
+
+func (s semaphoreHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	select {
+	case s.semaphore <- struct{}{}:
+		defer func() { <-s.semaphore }()
+		s.Handler.ServeHTTP(w, req)
+	default:
+		code := http.StatusServiceUnavailable
+		http.Error(w, http.StatusText(code), code)
+		return
+	}
 }
